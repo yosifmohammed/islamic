264c7b4c298891a49b54c433ba48d78c24ebfc51
yosifmohammed <ym7999322@gmail.com>
2021-06-15 19:08:34 +0200

    added splash screen for ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'homePage.dart';
 
 void main(){
   runApp(MyApp());
@@ -8,6 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
